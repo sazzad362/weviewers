e0def3b520b115004feda8a36ccd7644ff0c5c54
Md Sazzad Hossain Salim <sazzad362@gmail.com>
2022-06-21 16:53:19 +0600

    Desktop mode dashboard done

--- js/app.js
+++ js/app.js
@@ -25,6 +25,31 @@
         });
     }
 
+    // FAQ
+    $("#accordion").on("hide.bs.collapse show.bs.collapse", e => {
+      $(e.target)
+        .prev()
+        .find("i:last-child")
+        .toggleClass("fa-angle-down fa-angle-up");
+    });
+
+    // One Page scroll
+    $('.scrollTo').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        $('.modal').modal('hide');
+        var target = $(this.hash);
+        var headerH='0';
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top-headerH+"px"
+          }, 1000);
+          $('.Offcanvas_menu_wrapper,.off_canvars_overlay').removeClass('active');
+          return false;
+        }
+      }
+    });
+
 
 	});// DOM Ready
 }(jQuery)); // IIFE
